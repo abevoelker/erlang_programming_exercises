4a3f9cda2b87ce8b1cfd65d4fbf779a59494cc0e
Abe Voelker <abe@abevoelker.com>
2013-01-03 23:22:45 -0600

    Exercise 3-1 complete

--- 3/examples.erl
+++ 3/examples.erl
@@ -1,8 +1,12 @@
 -module(examples).
--export([sum/1]).
+-export([sum/1,sum/2]).
 
 % exercise 3-1
 
 sum(N) when N > 0 -> sum_acc(N, 0).
 sum_acc(0, Carry) -> Carry;
 sum_acc(N, Carry) -> sum_acc(N - 1, Carry + N).
+
+sum(N, M)                         -> sum_acc(N, M, 0).
+sum_acc(N, M, Carry) when N =:= M -> Carry + M;
+sum_acc(N, M, Carry) when N < M   -> sum_acc(N, M - 1, Carry + M).
