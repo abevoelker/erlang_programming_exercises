7dbd5d19f329aed9f24fba4365cde59cc42b8034
Abe Voelker <abe@abevoelker.com>
2013-01-04 00:56:32 -0600

    Exercise 3-3 complete

--- 3/examples.erl
+++ 3/examples.erl
@@ -1,5 +1,10 @@
 -module(examples).
--export([sum/1,sum/2,create/1,reverse_create/1]).
+-export([
+         sum/1, sum/2,
+         create/1,
+         reverse_create/1,
+         print_integers/1,
+         print_even_integers/1]).
 
 % exercise 3-1
 
@@ -23,3 +28,18 @@ create(N, List)      -> create(N-1, [N|List]).
 reverse_create(N) when N > 0            -> reverse_create(N, 1, []).
 reverse_create(N, M, List) when N =:= M -> [M|List];
 reverse_create(N, M, List)              -> reverse_create(N, M+1, [M|List]).
+
+% exercise 3-3
+
+print_integers(N) when N > 0 -> print_integers(N,1).
+print_integers(N, M) when N >= M ->
+  io:format("Number:~p~n", [M]),
+  print_integers(N, M+1);
+print_integers(_N, _M) -> ok.
+
+print_even_integers(N) when N > 0 -> print_even_integers(N,1).
+print_even_integers(N, M) when M rem 2 =:= 0 ->
+  io:format("Number:~p~n", [M]),
+  print_even_integers(N, M+1);
+print_even_integers(N, M) when M >= N -> ok;
+print_even_integers(N, M)             -> print_even_integers(N, M+1).
